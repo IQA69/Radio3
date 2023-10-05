0aab48b260148fd244e502d83c9384615ad5497f
IQA69 <charlesdward@mail.ru>
2023-10-05 21:01:32 +0300

    add Lombok

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,15 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+    private int maxStation;
+    private boolean on;
+}
